c8b6e60322819862610ba9d73e616ec20e07641d
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-08-06 17:59:37 -0500

    Insertando clientes a la base de datos

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -34,6 +34,40 @@
 
             return;
         }
+
+        //Crear un objeto con la información
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id: Date.now()
+        }
+
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente){
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function(){
+            imprimirAlerta('Hubo un error', 'error');
+        }
+
+        transaction.oncomplete = function(){
+            imprimirAlerta('Cliente agregado correctamente');
+
+            setTimeout(()=>{
+                window.location.href = 'index.html';
+            }, 2000);
+        }
+
+
     }
 
     function imprimirAlerta(mensaje, tipo){
