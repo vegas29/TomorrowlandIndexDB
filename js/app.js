aa06b3e2cb51119a84add6c3387157f0a8f456ca
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-08-06 19:07:36 -0500

    Funcionalidad eliminar listo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function(){
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -8,7 +9,10 @@
         if(window.indexedDB.open('crm', 1)){
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
+
 
     //Crea la base de datos de IndexDB
 
@@ -56,7 +60,7 @@
                 if(cursor){
                     const {nombre, empresa, email, telefono, id} = cursor.value;
 
-                    const listadoClientes = document.querySelector('#listado-clientes');
+                    
                     listadoClientes.innerHTML += ` 
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -71,7 +75,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
@@ -79,6 +83,27 @@
                     cursor.continue();
                 }else{
                     console.log('No hay mas registros...');
+                }
+            }
+        }
+    }
+
+    function eliminarRegistro(e){
+        if(e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+            const confirmar = confirm('¿Desear eliminar este cliente?');
+            if(confirmar){
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function(){
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function(){
+                    imprimirAlerta('Hubo un error', 'error');
                 }
             }
         }
